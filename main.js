f8b1c9826928f06aeaf09d0181f6a334d3d6611b
Sharky <71050482+SharkyLai@users.noreply.github.com>
2020-09-21 11:17:26 +0800

    updated formatting

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ var gameData = {
 
 function format(amount) {
   let power = Math.floor(Math.log10(amount));
-  let mantissa = amount * Math.pow(10, power);
+  let mantissa = amount / Math.pow(10, power);
   if (power < 3) return amount.toFixed(2);
   return mantissa.toFixed(2) + "e" + power
 }
