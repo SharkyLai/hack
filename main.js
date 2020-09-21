f328b3e19abcf1685d603da320a61c21dfda40c5
Sharky <71050482+SharkyLai@users.noreply.github.com>
2020-09-21 13:03:20 +0800

    a ton of updates + balance

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var gameData = {
     opponentLevel: 1,
     opponentHealth: 1,
     opponentRewards: 5,
+    wpw: 1
 }
 
 
@@ -34,9 +35,9 @@ function format(amount) {
 
 function levelUp() {
     gameData.opponentLevel = gameData.opponentLevel + 1;
-    gameData.opponentHealth = Math.pow(gameData.opponentLevel, 2.75);
+    gameData.opponentHealth = Math.pow(20, gameData.opponentLevel);
     gameData.Stellar = gameData.opponentRewards + gameData.Stellar;
-    gameData.opponentRewards = Math.pow(gameData.opponentLevel, 2.5);
+    gameData.opponentRewards = Math.pow(14, gameData.opponentLevel);
     document.getElementById("opponentLevel").innerHTML = "Level: " + gameData.opponentLevel;
     document.getElementById("opponentHealth").innerHTML = "HP: " + format(gameData.opponentHealth);
     document.getElementById("Stellar").innerHTML = "You have " + format(gameData.Stellar) + " Stellar.";
@@ -56,6 +57,50 @@ console.log(randomString(newsArray));
 var randomNews = randomString(newsArray);
 
 document.getElementById("randomNews").innerHTML = "Breaking news: " + randomNews;
+
+var words_1to100 = ["cookie", "string", "array", "python", "DDoS", "denial", "service", "variable", "constant", "function", "brute", "force", "framework", "script", "code", "spaghetti", "error", "html", "javascript", "hacker", "firewall", "css"];
+
+var wordAmount = gameData.opponentHealth / gameData.wpw
+
+
+var hackArray = [];
+var finalHackArray = [];
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+function generateHackArray() {
+  if (gameData.opponentLevel < 101) {
+      if (words_1to100.length > wordAmount) {
+          hackArray = shuffle(words_1to100);
+      } else {
+         for (i = 0; i < (wordAmount / words_1to100.length); i + 1) {
+         hackArray = shuffle(words_1to100);
+         finalHackArray = hackArray.concat(hackArray);
+      }
+    }  
+    return finalHackArray;
+ }
+}
+
+
+generateHackArray();
 
 
 // updateGUI();
